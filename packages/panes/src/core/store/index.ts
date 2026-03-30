be5a2efca1e1b355588890ccee6446ac11688881
Satya Patel <satyapatel111@gmail.com>
2026-03-29 18:41:27 -0700

    Checkpoint - store regenerated

---
+++ packages/panes/src/core/store/index.ts
@@ -0,0 +1,7 @@
+export { createWorkspaceStore } from "./store";
+export type {
+	CreatePaneInput,
+	CreateTabInput,
+	CreateWorkspaceStoreOptions,
+	WorkspaceStore,
+} from "./store";
---
+++ packages/panes/src/index.ts
@@ -0,0 +1,15 @@
+export { createWorkspaceStore } from "./core/store";
+export type {
+	CreatePaneInput,
+	CreateTabInput,
+	CreateWorkspaceStoreOptions,
+	WorkspaceStore,
+} from "./core/store";
+export type {
+	LayoutNode,
+	Pane,
+	SplitDirection,
+	SplitPosition,
+	Tab,
+	WorkspaceState,
+} from "./types";
